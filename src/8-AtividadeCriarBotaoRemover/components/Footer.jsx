c04d9e3e8da13a4464648f25359f3c671d1a44c5
PedroCassunde <>
2024-08-02 16:01:07 -0400

    Componentes Header e Footer

---
+++ src/8-AtividadeCriarBotaoRemover/components/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer({desenvolvedor}){
+    return(
+<footer>{desenvolvedor}</footer>
+
+    )
+};
--- src/8-AtividadeCriarBotaoRemover/index.jsx
+++ src/8-AtividadeCriarBotaoRemover/index.jsx
@@ -4,13 +4,16 @@
 // Passe o identificador (`id`) do item como parâmetro para `removerItemPedidos`, o mesmo que você usa no `map`.
 // No conteúdo do botão, coloque o texto "Remover".
 
-import { useState } from "react";
+import Header from "./components/Header"
+import Footer from "./components/Footer"; 
 
 export default function Home() {
 
   return (
     <div>
-         
+        <Header title={"SECRETÁAAAAAAAAAAAARIA, que trabalha o dia inteiro comigo"}/>
+        <Footer desenvolvedor={"Porque eu sou seu esquema preferido"}/>
     </div>
+
   );
 }
